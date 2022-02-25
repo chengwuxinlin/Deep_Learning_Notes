7b65f0eebadbcaebb3774c95575d1c07dc2f8119
chengwuxinlin <47012472+chengwuxinlin@users.noreply.github.com>
2022-02-24 20:46:40 -0500

    Update and rename AutoGluon_titanic_example.py to AutoGluon_cali_house_example.py

--- AutoGluon_titanic_example.py
+++
@@ -1,9 +0,0 @@
-from autogluon.tabular import TabularDataset, TabularPredictor
-#training
-trian_data = TabularDataset('train.csv')
-id, label = 'PassengerId', 'Survived'
-predictor = TabularPredictor(label = label).fit(train_data.drop(columns = [id]))
-
-import pandas as pd
-test_data = TabularDataset('test.csv')
-preds = predictor.predict(test_data.drop(columns = [id]))
